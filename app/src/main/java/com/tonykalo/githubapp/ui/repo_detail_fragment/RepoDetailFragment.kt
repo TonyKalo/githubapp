64d863ae29285f3f2177b4f5ab7d245f62a3d2af
Toni <tonikalogjeracinnamon@gmail.com>
2020-07-31 12:49:35 +0200

    Finish: repo detail fragment

--- app/src/main/java/com/tonykalo/githubapp/ui/repo_detail_fragment/RepoDetailFragment.kt
+++ app/src/main/java/com/tonykalo/githubapp/ui/repo_detail_fragment/RepoDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.tonykalo.githubapp.ui.repo_detail_fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,15 +10,16 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.tonykalo.githubapp.R
 import com.tonykalo.githubapp.ui.repo_detail_fragment.data.network.pojo.RepoResponse
-import com.tonykalo.githubapp.utils.extensions.log
-import com.tonykalo.githubapp.utils.extensions.toast
+import com.tonykalo.githubapp.utils.extensions.formatServerDateToDate
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_repo_detail.*
+import kotlinx.android.synthetic.main.row_user.*
 import javax.inject.Inject
-import kotlinx.android.synthetic.main.fragment_search.*
-import kotlinx.android.synthetic.main.fragment_user_detail.*
+
 
 class RepoDetailFragment : DaggerFragment() {
 
@@ -34,12 +37,12 @@ class RepoDetailFragment : DaggerFragment() {
         getArgs()
         setObservers()
         setOnClickListeners()
-//        initLoader()
+        initLoader()
     }
 
-//    private fun initLoader() {
-//        srlSearch.setOnRefreshListener { mViewModel.getData() }
-//    }
+    private fun initLoader() {
+        srlRepoDetail.setOnRefreshListener { mViewModel.getData() }
+    }
 
     private fun setOnClickListeners() {
         btnBack.setOnClickListener { findNavController().popBackStack() }
@@ -48,18 +51,39 @@ class RepoDetailFragment : DaggerFragment() {
     private fun setObservers() {
         mViewModel.repoData.observe(viewLifecycleOwner, Observer { updateUI(it) })
         mViewModel.handleError.observe(viewLifecycleOwner, Observer { showSnackbar(it) })
-        mViewModel.showLoader.observe(viewLifecycleOwner, Observer { })
+        mViewModel.showLoader.observe(viewLifecycleOwner, Observer { showSwipeLoader (it)})
     }
     private fun getArgs() {
         mViewModel.setRepoUrl(arguments?.get("repoUrl") as String)
-        arguments?.get("repoUrl")!!.log()
     }
 
     private fun updateUI(repoData: RepoResponse) {
-        requireContext().toast(repoData.full_name)
+        repoData.apply {
+            tvRepoName.text = name
+            tvLanguage.text = language ?: "-"
+            tvCreated.text = created_at.formatServerDateToDate()
+            tvUpdated.text = updated_at.formatServerDateToDate()
+            tvWatchers.text = watchers_count.toString()
+            tvForks.text = forks_count.toString()
+            tvIssues.text = open_issues_count.toString()
+            Glide.with(requireContext()).asBitmap().load(owner.avatar_url).placeholder(R.drawable.github_logo).into(civOwner)
+            tvOwner.text = owner.login
+            btnExternal.setOnClickListener { openInExternalBrowser(html_url) }
+            incOwner.setOnClickListener { findNavController().navigate(RepoDetailFragmentDirections.actionRepoDetailFragmentToUserDetailFragment(owner.url)) }
+        }
+
+    }
+
+    private fun openInExternalBrowser(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 
     private fun showSnackbar(msg: String) {
         Snackbar.make(requireActivity().findViewById(android.R.id.content), msg, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun showSwipeLoader(show: Boolean) {
+        srlRepoDetail.isRefreshing = show
     }
 }
